e3e0604fe42463ae6e970dc6cf679aecd9e8bdc1
ncy <1136962813@qq.com>
2021-05-13 14:54:09 +0800

    bencode解码

--- test/bencoding.cpp
+++ test/bencoding.cpp
@@ -20,5 +20,27 @@ int main ()
     blist.push(blist);
 
     printf("dict:%s\nlist:%s\n", bdict.encode().c_str(), blist.encode().c_str());
+
+	BValue query;
+	query["t"] = "aa";
+	query["y"] = "q";
+	query["q"] = "ping";
+	BValue body;
+	body["id"] = "abcdefghij0123456789";
+	query["a"] = body;
+
+	BValue response;
+	int pos = 0;
+
+	response.decode("i123456e", pos);
+	pos = 0;
+	response.decode("3:ddd", pos);
+	pos = 0;
+	response.decode("l3:ddde", pos);
+	pos = 0;
+	response.decode("d1:rd2:id20:mnopqrstuvwxyz123456e1:t2:aa1:y1:re", pos);
+
+	printf("query:%s\n", query.encode().c_str());
+	printf("response:%s\n", response.encode().c_str());
 	return 0;
 }
